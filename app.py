2d8c193e2642d5991a3b43959e0280107e87c696
Frank9moh <107345416+Frank9moh@users.noreply.github.com>
2024-07-27 20:30:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import html2text
 import logging
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 logging.basicConfig(level=logging.DEBUG)
 
